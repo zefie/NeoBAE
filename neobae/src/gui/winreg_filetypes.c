35d80f95bbb87ae9f68b06a64c0d93115078406a
zefie <zefie@zefie.net>
2025-12-26 18:12:21 -0500

    winreg_filetypes: make window color more natural

--- neobae/src/gui/winreg_filetypes.c
+++ neobae/src/gui/winreg_filetypes.c
@@ -359,7 +359,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     wc.lpfnWndProc = WndProc;
     wc.hInstance = hInstance;
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+    wc.hbrBackground = (HBRUSH)(COLOR_BTNFACE+1);
     wc.lpszClassName = _T("zefidiHelperClass");
 
     if(!RegisterClassEx(&wc)) return -1;
